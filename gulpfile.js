0602be56f0a19521c16ea838ce8cfa176d8db908
Alex <alexmojicamtz@gmail.com>
2019-05-03 10:12:59 +0200

    09Git Timeout

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,2 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-
-gulp.task('default', function(){
-  console.log("Bieeeeen - he creado una task de Gulp.");
-});
-
-gulp.task('html', function(){
-  console.log("Imagina algo bueno para html");
-});
-
-gulp.task('watch', function(){
-  watch('./app/index.html', function(){
-    gulp.start('html');
-  });
-});
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
